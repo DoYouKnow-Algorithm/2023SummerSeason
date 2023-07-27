7d09f45cefc14a76ab45d0785a7eeac75359fbe6
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-07-27 22:55:26 +0900

    3주 1일차

---
+++ ayuriK152/3주차/boj5568.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmStudy
+{
+    class boj5568
+    {
+        static HashSet<string> list = new HashSet<string>();    // Hash는 중복값을 허용하지 않는 원리를 이용, 따로 검사하는 수고를 덜 수 있음.
+        static int n;
+        static int k;
+        static string[] nums;
+        static bool[] check;    // 문자열 중복선택 방지용 bool배열
+        public static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            nums = new string[n];
+            check = new bool[n];
+            for (int i = 0; i < n; i++)
+                nums[i] = Console.ReadLine();
+            Assemble(0, "");
+            Console.WriteLine(list.Count);
+        }
+
+        static void Assemble(int len, string str)   // 재귀식으로 백트래킹 구현
+        {
+            if (len == k)
+                list.Add(str);
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (check[i])
+                        continue;
+                    check[i] = true;
+                    Assemble(len + 1, str + nums[i]);
+                    check[i] = false;
+                }
+            }
+        }
+    }
+}
