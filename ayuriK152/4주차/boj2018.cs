1b383ca0bcc15ad7ed94391fe3d4c31dde95f1f0
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-08-03 23:03:29 +0900

    4주 1일차

---
+++ ayuriK152/4주차/boj2018.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AlgorithmStudy
+{
+    class boj2018
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 1;
+            for (int i = 1; i <= n / 2; i++)
+            {
+                for (int j = i + 1; j <= n / 2 + 1; j++)
+                {
+                    int temp = (j + i) % 2 == 0 ? (j + i) * (j - (j + i) / 2) + (j + i) / 2 : (j + i) * (j - (j + i) / 2);  // 한번 계산하고 계속 써먹기
+                    if (temp == n)     // n과 같은 경우 count를 늘리고 두번째 포인터 반복문 종료
+                    {
+                        count++;
+                        break;
+                    }
+                    else if (temp > n)      // n을 초과하는 경우 그냥 종료
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
